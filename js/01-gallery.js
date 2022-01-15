a3410ba9b37482a549afed61174bfff73af97955
nicole4875 <90067726+NicoleGutsul@users.noreply.github.com>
2022-01-15 20:59:04 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,34 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// const galleryRef = document.querySelector(".gallery");
-
-// const galleryMarkup = galleryItems
-//     .map(({ preview, original, description }) => {
-//         return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
-//     })
-//     .join("");
-// galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
-
-// galleryRef.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     if (e.target !== e.currentTarget) {
-//         const img = e.target.dataset.source;
-
-//         const instance = basicLightBox.create(`
-//         <img src="${img}" width="1280">
-//         `);
-     
-//         instance.show();
-
-//         galleryRef.addEventListener("keydown", (e) => {
-//             if (e.key === "Escape") {
-//                 instance.close();
-//             }
-//         })
-//     }
-// })
-
 const galleryRef = document.querySelector(".gallery");
 
 const galleryMarkup = galleryItems
